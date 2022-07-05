6212cc3850db0dc4932a4672732abed2562cce01
Dagnm <dagnachew16@gmail.com>
2022-07-05 09:15:13 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "dagn.h"
+/**
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('d');
+	_putchar('a');
+	_putchar('g');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+These is exercise for function nested loops in C
